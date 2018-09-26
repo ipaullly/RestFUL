79064569aca8593b6488fd55eb6e35d079d4e23c
ipaully <ipaully22@gmail.com>
2018-09-26 05:48:53 +0300

    [FEAT] Add GET routes

--- api.py
+++ api.py
@@ -1,9 +1,20 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+languages = [{'name' : 'Javascript'}, {'name' : 'Python'}, {'name' : 'Ruby'}]
+
 @app.route('/', methods=['GET'])
 def test():
     return jsonify({'prompt' : 'Welcome to restful api'})
+
+@app.route('/lang', methods=['GET'])
+def returnAll():
+    return jsonify({'languages' : languages})
+
+@app.route('/lang/<string:name>', methods=['GET'])
+def returnOne(name):
+    langs = [language for language in languages if language['name'] == name]
+    return jsonify({'language' : langs[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
